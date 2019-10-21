f36a89c6a51db888cfbb8d59e20e01cd2a9cd400
Alan K <akillian@outlook.com>
2019-10-21 14:27:08 -0400

    Update build.js.

--- scripts/build.js
+++ scripts/build.js
@@ -60,13 +60,12 @@ require("rimraf")("./dist", function() {
               await writeFile(`dist/css/${cssFile}`, uglified);
             });
 
-            return `Copied /src/css files to /dist/css directory successfully === 
+            return `Copied /src/css files to /dist/css directory successfully 
                     Uglified CSS file(s) Successfully!!! ======= ${checkMark}`;
           } else if (!readDirectory.length) {
             return `Alert. /css directory empty ${warning}
              ====== End CSS Uglify. =====`;
           } // end if/else
-         
         } catch (err) {
           console.log("ERROR:", err);
         }
